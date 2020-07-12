22f1ad42a40ca4ceb3255c6a456d5934860dddbf
Maria Noriega <maria.noriega@utec.edu.pe>
2020-07-12 02:40:10 -0500

    models in R

--- RPathAnalysisModel01.R
+++ RPathAnalysisModel01.R
@@ -1,22 +1,25 @@
 library(readr)
 library(lavaan)
 library(semPlot)
+library(ggplot2)
 library(ggcorrplot)
+library(ggpubr)
+library(GGally)
 
 data01 <- read_csv("https://public.minsky.cc/maria/market_channel_factors_db/model/01ModelDataframeOfWhitePotato.csv")
 
-model <- "# direct effect
-          farmGatePricePerKg ~ c1*districtTimeToProvinceCapitalOfRegion + c2*provinceCapitalToLimaMarket + c3*informalPriceInformation + c4*formalPriceInformation + c5*informalMetereologicalInformation + c6*formalMetereologicalInformation + c7*volumeOfDistrict + c8*soldProduction + c9*yield + c10*associative + c11*pointsOfSale
+model01 <- "# direct effect
+          farmGatePricePerKg ~ c0*districtTimeToLimaMarket + c1*districtTimeToProvinceCapitalOfRegion + c3*informalPriceInformation + c4*formalPriceInformation + c5*informalMetereologicalInformation + c6*formalMetereologicalInformation + c7*volumeOfDistrict + c8*soldProduction + c9*yield + c10*associative + c11*pointsOfSale
           # mediators
-          marketSize ~ a11*districtTimeToProvinceCapitalOfRegion + a12*provinceCapitalToLimaMarket + a13*informalPriceInformation + a14*formalPriceInformation + a15*informalMetereologicalInformation + a16*formalMetereologicalInformation + a17*volumeOfDistrict + a18*soldProduction + a19*yield + a1a*associative + a1b*pointsOfSale
-          marketChannelDirectedness ~ a21*districtTimeToProvinceCapitalOfRegion + a22*provinceCapitalToLimaMarket + a23*informalPriceInformation + a24*formalPriceInformation + a25*informalMetereologicalInformation + a26*formalMetereologicalInformation + a27*volumeOfDistrict + a28*soldProduction + a29*yield + a2a*associative + a2b*pointsOfSale
+          marketSize ~ a10*districtTimeToLimaMarket + a11*districtTimeToProvinceCapitalOfRegion + a13*informalPriceInformation + a14*formalPriceInformation + a15*informalMetereologicalInformation + a16*formalMetereologicalInformation + a17*volumeOfDistrict + a18*soldProduction + a19*yield + a1a*associative + a1b*pointsOfSale
+          marketChannelDirectedness ~ a20*districtTimeToLimaMarket + a21*districtTimeToProvinceCapitalOfRegion + a23*informalPriceInformation + a24*formalPriceInformation + a25*informalMetereologicalInformation + a26*formalMetereologicalInformation + a27*volumeOfDistrict + a28*soldProduction + a29*yield + a2a*associative + a2b*pointsOfSale
           # indirect effect
           farmGatePricePerKg ~ b1*marketSize + b2*marketChannelDirectedness
           # covariance
           #marketSize ~~ marketChannelDirectedness
           # mediation throught market size
+          i_10 := a10*b1
           i_11 := a11*b1
-          i_12 := a12*b1
           i_13 := a13*b1
           i_14 := a14*b1
           i_15 := a15*b1
@@ -27,8 +30,8 @@ model <- "# direct effect
           i_1a := a1a*b1
           i_1b := a1b*b1
           # mediation throught market channel directedness
+          i_20 := a20*b2
           i_21 := a21*b2
-          i_22 := a22*b2
           i_23 := a23*b2
           i_24 := a24*b2
           i_25 := a25*b2
@@ -40,9 +43,17 @@ model <- "# direct effect
           i_2b := a2b*b2
           "
 
-fit01 <- sem(model, data = data01, estimator="WLSM")
+fit01 <- sem(model01, data = data01, estimator="WLSM")
 summary(fit01, fit.measures=TRUE)
 fitMeasures(fit01, c("cfi","rmsea","srmr", "cfi.robust", "rmsea.robust"))
 varTable(fit01)
 semPaths(fit01, what='std', nCharNodes=6, sizeMan=10,
          edge.label.cex=1.25, curvePivot = TRUE, fade=FALSE)
+
+ggcorrplot(fitted(fit01, type="cov")$cov, type="upper")
+corrplot(cor(data01[, 11:25]))
+plot(fit01)
+
+
+ggpairs(data01[, 11:25])
+ggpairs(data01[, 11:22])
